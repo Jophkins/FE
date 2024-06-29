f147daaabfe4ab40cde1c3ff7b1b78bb47e44cb8
Seth <sergebrew@gmail.com>
2024-06-29 10:00:52 -0600

    navbar widget, UI kit component, development patterns

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles/index.scss';
-import {Link} from 'react-router-dom';
 import {useTheme} from "app/providers/ThemeProvider";
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
@@ -15,9 +15,8 @@ const App = () => {
 
     return (
         <div className={classNames("App", {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
 
             <div className="best">BEST</div>
 
